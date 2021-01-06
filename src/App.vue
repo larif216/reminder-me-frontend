06db836e05e093584f1f685707c7ff3f88dd1ea3
larif216 <muhamadlutfiarif10@gmail.com>
2021-01-07 04:48:38 +0700

    Change title and and navbar

--- src/App.vue
+++ src/App.vue
@@ -1,175 +1,178 @@
 <template>
-    <div class="container pt-4 pb-4">
-        <div class="container content">
-            <h1 class="pb-4 text-center">
-                Deadline Bersama
-            </h1>
-            <div class="col-md-12 mb-3 text-center">
-                <btn-create @showModalCreate="showModalCreate"/>
-            </div>
-            <div class="row">
-                <b-col lg="6 mb-4">
-                    <b-form-group
-                        label="Per page"
-                        label-cols-sm="6"
-                        label-cols-md="4"
-                        label-cols-lg="2"
-                        label-align-sm="left"
+    <div id="app">
+        <navbar/>
+        <div class="container pt-4 pb-4">
+            <div class="container content">
+                <h1 class="pb-4 text-center">
+                    Deadline Bersama
+                </h1>
+                <div class="col-md-12 mb-3 text-center">
+                    <btn-create @showModalCreate="showModalCreate"/>
+                </div>
+                <div class="row">
+                    <b-col lg="6 mb-4">
+                        <b-form-group
+                            label="Per page"
+                            label-cols-sm="6"
+                            label-cols-md="4"
+                            label-cols-lg="2"
+                            label-align-sm="left"
+                            label-size="sm"
+                            label-for="perPageSelect"
+                            class="mb-0"
+                            >
+                            <b-form-select
+                                v-model="perPage"
+                                id="perPageSelect"
+                                size="sm"
+                                :options="pageOptions"
+                            ></b-form-select>
+                        </b-form-group>
+                    </b-col>
+                    <b-col lg="6 mb-4">
+                        <b-form-group
+                        label="Find"
+                        label-cols-sm="3"
+                        label-align-sm="right"
                         label-size="sm"
-                        label-for="perPageSelect"
+                        label-for="filterInput"
                         class="mb-0"
                         >
-                        <b-form-select
-                            v-model="perPage"
-                            id="perPageSelect"
+                            <b-input-group size="sm">
+                                <b-form-input
+                                v-model="filter"
+                                type="search"
+                                id="filterInput"
+                                placeholder="Type to Search"
+                                ></b-form-input>
+                                <b-input-group-append>
+                                <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
+                                </b-input-group-append>
+                            </b-input-group>
+                        </b-form-group>
+                    </b-col>
+                    <div class="col-md-12">
+                        <b-table id="table" responsive filter-debounce="500" striped :current-page="currentPage" :per-page="perPage" hover :items="items" :fields="fields" show-empty :filter="filter" @filtered="onFiltered" head-variant="dark"> 
+                            <template v-slot:cell(due_date_time)="data">
+                                {{ formatDate(data.item.due_date_time) }}
+                            </template>
+                            <template v-slot:cell(action)="data">
+                                <btn-delete class="ml-1 mb-1" :id="data.item.id" @deleteData="deleteData(data.item.id)"/>
+                                <btn-update class="ml-1 mb-1" :id="data.item.id" @showModalUpdate="showModalUpdate(data.item)"/>
+                            </template>
+                        </b-table>   
+                    </div>
+                    <div class="col-md-12">
+                        <b-pagination
+                            v-model="currentPage"
+                            :total-rows="totalRows"
+                            :per-page="perPage"
+                            align="fill"
                             size="sm"
-                            :options="pageOptions"
-                        ></b-form-select>
-                    </b-form-group>
-                </b-col>
-                <b-col lg="6 mb-4">
-                    <b-form-group
-                    label="Find"
-                    label-cols-sm="3"
-                    label-align-sm="right"
-                    label-size="sm"
-                    label-for="filterInput"
-                    class="mb-0"
-                    >
-                        <b-input-group size="sm">
-                            <b-form-input
-                            v-model="filter"
-                            type="search"
-                            id="filterInput"
-                            placeholder="Type to Search"
-                            ></b-form-input>
-                            <b-input-group-append>
-                            <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-                            </b-input-group-append>
-                        </b-input-group>
-                    </b-form-group>
-                </b-col>
-                <div class="col-md-12">
-                    <b-table id="table" responsive filter-debounce="500" striped :current-page="currentPage" :per-page="perPage" hover :items="items" :fields="fields" show-empty :filter="filter" @filtered="onFiltered" head-variant="dark"> 
-                         <template v-slot:cell(due_date_time)="data">
-                            {{ formatDate(data.item.due_date_time) }}
-                        </template>
-                        <template v-slot:cell(action)="data">
-                            <btn-delete class="ml-1 mb-1" :id="data.item.id" @deleteData="deleteData(data.item.id)"/>
-                            <btn-update class="ml-1 mb-1" :id="data.item.id" @showModalUpdate="showModalUpdate(data.item)"/>
-                        </template>
-                    </b-table>   
-                </div>
-                <div class="col-md-12">
-                    <b-pagination
-                        v-model="currentPage"
-                        :total-rows="totalRows"
-                        :per-page="perPage"
-                        align="fill"
-                        size="sm"
-                        class="my-0"
-                        ></b-pagination>
-                </div>
-                <b-modal ref="modal-update" title="Update Data" hide-footer>
-                    <div>
-                        <b-form @submit="updateData">
-
-                            <b-form-group id="input-group-1" label="Nama" label-for="input-1">
-                                <b-form-input
-                                    id="input-1"
-                                    v-model="updateFields.name"
-                                    required
-                                ></b-form-input>
-                            </b-form-group>
-
-                            <b-form-group id="input-group-2" label="Matkul" label-for="input-2">
-                                <b-form-input
-                                    id="input-2"
-                                    v-model="updateFields.matkul"
-                                    required
-                                ></b-form-input>
-                            </b-form-group>
-
-                            <b-form-group id="input-group-3" label="Deskripsi" label-for="input-3">
-                                <b-form-input
-                                    id="input-3"
-                                    v-model="updateFields.description"
-                                    required
-                                ></b-form-input>
-                            </b-form-group>
-
-                            <b-form-group id="input-group-4" label="Tanggal Deadline" label-for="input-4">
-                                <b-form-datepicker
-                                    id="input-4"
-                                    v-model="updateFields.dueDate"
-                                    required
-                                    ></b-form-datepicker>
-                            </b-form-group>
-
-                            <b-form-group id="input-group-5" label="Waktu Deadline" label-for="input-5">
-                                <b-form-timepicker
-                                    v-model="updateFields.dueTime"
-                                    locale="en"
-                                ></b-form-timepicker>
-                            </b-form-group>
-
-                            <b-button variant="primary" @click.prevent="updateData()">Update</b-button>
-                        </b-form>
-                        <!-- <b-card class="mt-3" header="Form Data Result">
-                        <pre class="m-0">{{ updateFields }}</pre>
-                        </b-card> -->
+                            class="my-0"
+                            ></b-pagination>
                     </div>
-                </b-modal>
-                <b-modal ref="modal-create" title="Create Data" hide-footer>
-                    <div>
-                        <b-form @submit="createData">
+                    <b-modal ref="modal-update" title="Update Data" hide-footer>
+                        <div>
+                            <b-form @submit="updateData">
 
-                            <b-form-group id="input-group-1" label="Nama" label-for="input-1">
-                                <b-form-input
-                                    id="input-1"
-                                    v-model="createFields.name"
-                                    required
-                                ></b-form-input>
-                            </b-form-group>
+                                <b-form-group id="input-group-1" label="Nama" label-for="input-1">
+                                    <b-form-input
+                                        id="input-1"
+                                        v-model="updateFields.name"
+                                        required
+                                    ></b-form-input>
+                                </b-form-group>
 
-                            <b-form-group id="input-group-2" label="Matkul" label-for="input-2">
-                                <b-form-input
-                                    id="input-2"
-                                    v-model="createFields.matkul"
-                                    required
-                                ></b-form-input>
-                            </b-form-group>
+                                <b-form-group id="input-group-2" label="Matkul" label-for="input-2">
+                                    <b-form-input
+                                        id="input-2"
+                                        v-model="updateFields.matkul"
+                                        required
+                                    ></b-form-input>
+                                </b-form-group>
 
-                            <b-form-group id="input-group-3" label="Deskripsi" label-for="input-3">
-                                <b-form-input
-                                    id="input-3"
-                                    v-model="createFields.description"
-                                    required
-                                ></b-form-input>
-                            </b-form-group>
+                                <b-form-group id="input-group-3" label="Deskripsi" label-for="input-3">
+                                    <b-form-input
+                                        id="input-3"
+                                        v-model="updateFields.description"
+                                        required
+                                    ></b-form-input>
+                                </b-form-group>
 
-                            <b-form-group id="input-group-4" label="Tanggal Deadline" label-for="input-4">
-                                <b-form-datepicker
-                                    id="input-4"
-                                    v-model="createFields.dueDate"
-                                    required
-                                    ></b-form-datepicker>
-                            </b-form-group>
+                                <b-form-group id="input-group-4" label="Tanggal Deadline" label-for="input-4">
+                                    <b-form-datepicker
+                                        id="input-4"
+                                        v-model="updateFields.dueDate"
+                                        required
+                                        ></b-form-datepicker>
+                                </b-form-group>
 
-                            <b-form-group id="input-group-5" label="Waktu Deadline" label-for="input-5">
-                                <b-form-timepicker
-                                    v-model="createFields.dueTime"
-                                    locale="en"
-                                ></b-form-timepicker>
-                            </b-form-group>
+                                <b-form-group id="input-group-5" label="Waktu Deadline" label-for="input-5">
+                                    <b-form-timepicker
+                                        v-model="updateFields.dueTime"
+                                        locale="en"
+                                    ></b-form-timepicker>
+                                </b-form-group>
 
-                            <b-button variant="primary" @click.prevent="createData()">Create</b-button>
-                        </b-form>
-                        <!-- <b-card class="mt-3" header="Form Data Result">
-                        <pre class="m-0">{{ createFields }}</pre>
-                        </b-card> -->
-                    </div>
-                </b-modal>
+                                <b-button variant="primary" @click.prevent="updateData()">Update</b-button>
+                            </b-form>
+                            <!-- <b-card class="mt-3" header="Form Data Result">
+                            <pre class="m-0">{{ updateFields }}</pre>
+                            </b-card> -->
+                        </div>
+                    </b-modal>
+                    <b-modal ref="modal-create" title="Create Data" hide-footer>
+                        <div>
+                            <b-form @submit="createData">
+
+                                <b-form-group id="input-group-1" label="Nama" label-for="input-1">
+                                    <b-form-input
+                                        id="input-1"
+                                        v-model="createFields.name"
+                                        required
+                                    ></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group id="input-group-2" label="Matkul" label-for="input-2">
+                                    <b-form-input
+                                        id="input-2"
+                                        v-model="createFields.matkul"
+                                        required
+                                    ></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group id="input-group-3" label="Deskripsi" label-for="input-3">
+                                    <b-form-input
+                                        id="input-3"
+                                        v-model="createFields.description"
+                                        required
+                                    ></b-form-input>
+                                </b-form-group>
+
+                                <b-form-group id="input-group-4" label="Tanggal Deadline" label-for="input-4">
+                                    <b-form-datepicker
+                                        id="input-4"
+                                        v-model="createFields.dueDate"
+                                        required
+                                        ></b-form-datepicker>
+                                </b-form-group>
+
+                                <b-form-group id="input-group-5" label="Waktu Deadline" label-for="input-5">
+                                    <b-form-timepicker
+                                        v-model="createFields.dueTime"
+                                        locale="en"
+                                    ></b-form-timepicker>
+                                </b-form-group>
+
+                                <b-button variant="primary" @click.prevent="createData()">Create</b-button>
+                            </b-form>
+                            <!-- <b-card class="mt-3" header="Form Data Result">
+                            <pre class="m-0">{{ createFields }}</pre>
+                            </b-card> -->
+                        </div>
+                    </b-modal>
+                </div>
             </div>
         </div>
     </div>
@@ -182,13 +185,15 @@ import moment from 'moment'
 import ButtonDelete from '@/components/ButtonDelete'
 import ButtonUpdate from '@/components/ButtonUpdate.vue'
 import ButtonCreate from './components/ButtonCreate.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
     name: "App",
     components: {
         'btn-delete': ButtonDelete,
         'btn-update': ButtonUpdate,
-        'btn-create': ButtonCreate
+        'btn-create': ButtonCreate,
+        'navbar': Navbar
     },
     mounted() {
         this.readData()
@@ -380,7 +385,7 @@ export default {
         background-color: white;
         box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
         border-radius: 10px;
-        padding: 80px 50px 80px 50px !important;
+        padding: 60px 50px 60px 50px !important;
     }
 
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg">
+            <b-navbar-brand href="#">
+                <img src="../assets/logo.png" width="30" height="30">
+            </b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item href="#">Reminder.me</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style>
+    nav {
+        background-color: #323F75 !important;
+    }
+
+    .navbar-light .navbar-nav .nav-link {
+        color: white !important;
+    }
+</style>
